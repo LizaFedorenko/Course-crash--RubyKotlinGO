6f44ee6c6459b85134ef034bf30555077f2605cb
LizaFedorenko <114235932+LizaFedorenko@users.noreply.github.com>
2024-11-03 22:12:30 +0200

    Add files via upload

---
+++ Student.rb
@@ -0,0 +1,43 @@
+require 'date'
+
+class Student 
+  @@students = []
+  attr_accessor :surname, :name, :date_of_birth, :age
+  def initialize(surname, name, date_of_birth)
+    @surname = surname
+    @name = name
+    @date_of_birth = date_of_birth
+    @age = calculate_age
+  end
+
+  def calculate_age
+    birth_date = Date.parse(@date_of_birth) 
+    age = Date.today.year - birth_date.year
+    age -= 1 if Date.today < birth_date.next_year(age)
+    raise ArgumentError, "Date is incorrect" if age < 0
+    age
+  end
+
+  def self.add_student(surname, name, date_of_birth)
+    student = Student.new(surname, name, date_of_birth)
+    raise ArgumentError, "This student is already in list" if @@students.any? { |student| 
+    student.surname == surname && 
+    student.name == name && 
+    student.date_of_birth == date_of_birth
+  }
+    @@students << student
+    student
+  end
+
+  def self.remove_student(surname, name, date_of_birth)
+    @@students.delete_if { |student| student.surname == surname && student.name == name && student.date_of_birth ==  date_of_birth}
+  end
+
+  def self.get_students_by_age(age)
+    @@students.select { |student| student.age == age }
+  end
+
+  def self.get_students_by_name(name)
+    @@students.select { |student| student.name == name }
+  end
+end
